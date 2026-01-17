e2a1d7423c9123fd64a6aedb0970eba9ceb94fa0
m4rkbello <markbello10041999@gmail.com>
2026-01-18 00:32:19 +0800

    ADD NEW COMPONENT AS NAVBAR

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,19 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "AIPATS BY M4RKBELLO" },
+    { name: "description", content: "Smart Feedback For Your Dream Job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')]">
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>Track Your Application & Resume Ratings!</h1>
+        <h2>Review your  submissions and check  AI-Powered feedback.</h2>
+      </div>
+    </section>
+  </main>;
 }
