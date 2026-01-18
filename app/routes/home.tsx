2af437f67d5f5362a8925f6e73fce663ff3d131d
m4rkbello <markbello10041999@gmail.com>
2026-01-18 10:35:22 +0800

    RESTRUCTURE AND REFACTOR THE HOME

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -15,26 +15,20 @@ export default function Home() {
   return <main className="bg-[url('/images/bg-main.svg')]">
     <Navbar />
     <section className="main-section">
-      <div className="page-heading">
+      <div className="page-heading py-16">
         <h1>Track Your Application & Resume Ratings!</h1>
         <h2>Review your  submissions and check  AI-Powered feedback.</h2>
       </div>
+        {resumes.length > 0 && (
+          <div className="resumes-section">
+            {/* map display */}
+            {resumes.map((resume: Resume, index: number) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        )
+
+        }
     </section>
-
-
-    {resumes.length > 0 && (
-      <div className="resumes-section">
-        {/* map display */}
-        {resumes.map((resume: Resume, index: number) => (
-          <ResumeCard key={resume.id} resume={resume} />
-        ))}
-      </div>
-    )
-
-    }
-
-
-
-
   </main>;
 }
